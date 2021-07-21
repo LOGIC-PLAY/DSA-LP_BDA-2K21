5bb7793ae67f60b0d6a2f57d351693539e38976d
Ampolu Nisa <63179438+AmpoluNisa03@users.noreply.github.com>
2021-07-21 22:41:48 +0530

    Update Exceptionally Odd.cpp

--- Nisha/Arrays/Exceptionally Odd.cpp
+++ Nisha/Arrays/Exceptionally Odd.cpp
@@ -11,20 +11,20 @@ public:
 
 int main()
 {
-int t;
-cin>>t;
-while(t--)
-{
-int n;
-cin>>n;
-int arr[n];
-for(int i=0;i<n;i++)
-{
-cin>>arr[i];
-}
-Solution ob;
-auto ans=ob.getOddOccurrence(arr,n);
-cout<<ans<<"\n";
-}
-return 0;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        auto ans=ob.getOddOccurrence(arr,n);
+        cout<<ans<<"\n";
+    }
+    return 0;
 }
