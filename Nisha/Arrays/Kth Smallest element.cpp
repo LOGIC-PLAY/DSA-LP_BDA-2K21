fa0b675434f7055436d2719dba3e92de951542f1
Ampolu Nisa <63179438+AmpoluNisa03@users.noreply.github.com>
2021-07-21 22:43:02 +0530

    Update Kth Smallest element.cpp

--- Nisha/Arrays/Kth Smallest element.cpp
+++ Nisha/Arrays/Kth Smallest element.cpp
@@ -13,21 +13,21 @@ class Solution{
 };
 int main()
 {
-int test_case;
-cin>>test_case;
-while(test_case--)
-{
-int number_of_elements;
-cin>>number_of_elements;
-int a[number_of_elements];
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
 
-for(int i=0;i<number_of_elements;i++)
-cin>>a[i];
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
 
-int k;
-cin>>k;
-Solution ob;
-cout<<ob.kthSmallest(a,0,number_of_elements-1,k)<<endl;
-}
-return 0;
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a,0,number_of_elements-1,k)<<endl;
+    }
+    return 0;
 }
