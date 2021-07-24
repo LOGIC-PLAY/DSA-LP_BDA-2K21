58d863728998539c417a15f0fad664f46c03e83b
Ampolu Nisa <63179438+AmpoluNisa03@users.noreply.github.com>
2021-07-24 22:07:16 +0530

    Add files via upload

---
+++ Nisha/Linked List/Detect loop in Linked List.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        if(head==NULL) return 0;
+        Node*slow=head;
+        Node*fast=slow->next;
+        while(fast!=slow)
+        {
+            if(fast==NULL || fast->next==NULL) return 0;
+            fast=fast->next->next;
+            slow=slow->next;
+            
+        }
+        return 1;
+        
+    }
+};
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+int n, num;
+cin>>n;
+
+Node *head, *tail;
+cin>>num;
+head=tail=new Node(num);
+
+for(int i=0;i<n-1;i++)
+{
+cin>>num;
+tail->next=new Node(num);
+tail=tail->next;
+]
+
+int pos;
+cin>>pos;
+loopHere(head,tail,pos);
+Solution ob;
+if(ob.detectLoop(head))
+cout<<"True\n";
+else
+cout<<"False\n";
+}
+return 0;
+}
